2c70f0cd17e6948bca69c0cb2ae9c4151f2c7b1d
avh <andreas.vanhelden@conceto.de>
2024-12-30 20:22:40 +0100

    main: ensure extras and linters are installed and treesitter extensions

---
+++ lua/plugins/extend-mason-linters.lua
@@ -0,0 +1,22 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      -- LSP
+      -- "dockerls",
+      -- "eslint",
+      -- "jsonls",
+      -- "marksman",
+      -- "prismals",
+      -- "taplo",
+      -- "vtsls",
+      -- linter
+      "eslint_d",
+      "hadolint",
+      "markdown-toc",
+      "markdownlint-cli2",
+      "shfmt",
+      "stylua",
+    },
+  },
+}
