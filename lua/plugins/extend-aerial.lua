22dde3cc3b803bdd69ff3ae6d3bf929f98348dc2
avh <andreas.vanhelden@conceto.de>
2025-01-01 01:51:11 +0100

    main: configure aerial window

--- lua/plugins/extend-aerial.lua
+++ lua/plugins/extend-aerial.lua
@@ -4,10 +4,29 @@ return {
   -- event = "LazyFile",
   opts = {
     autojump = true,
+    sort = "name",
     filter_kind = {
       "Class",
       "Constructor",
+      "Enum",
+      "Function",
+      "Interface",
       "Method",
+      "Struct",
     },
+
+    layout = {
+      max_width = 0.5,
+      -- width = nil,
+      min_width = 0.2,
+      -- When the symbols change, resize the aerial window (within min/max constraints) to fit
+      resize_to_content = true,
+    },
+    float = {
+      relative = "window",
+    },
+  },
+  keys = {
+    { "<leader>cs", "<cmd>AerialToggle<cr>", desc = "Aerial (Symbols)" },
   },
 }
