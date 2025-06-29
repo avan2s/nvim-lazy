6e47ee013b0ff97412625a9a17640e17ac5eaff6
avh <andreas.vanhelden@conceto.de>
2025-06-29 19:38:50 +0200

    20250629-replace-nvim: add code action keybinding with alt+enter

---
+++ lua/plugins/extend-lsp.lua
@@ -0,0 +1,51 @@
+-- LSP keymaps
+-- https://www.lazyvim.org/plugins/lsp#%EF%B8%8F-customizing-lsp-keymaps
+return {
+  "neovim/nvim-lspconfig",
+  opts = function()
+    local key = require("lazyvim.plugins.lsp.keymaps").get()
+    -- disable a keymap example (comment it in if you want to disable):
+    -- key[#key + 1] = { "gr", false }
+    -- key[#key + 1] = { "<leader>gi", vim.lsp.buf.implementation, desc = "Goto Implementation" }
+    key[#key + 1] = { "<M-CR>", vim.lsp.buf.code_action, desc = "Code Action", mode = { "n", "v" }, has = "codeAction" }
+    -- key[#key + 1] = { "<leader>uu", vim.lsp.buf.references, desc = "References", nowait = true }
+    -- key[#key + 1] = { "<leader>rn", vim.lsp.buf.rename, desc = "rename", has = "rename" }
+    -- key[#key + 1] = { "gu", vim.lsp.buf.references, desc = "References", nowait = true }
+    -- key[#key + 1] = {
+    --   "<leader>rf",
+    --   function()
+    --     Snacks.rename.rename_file()
+    --   end,
+    --   desc = "Rename File",
+    --   mode = { "n" },
+    --   has = { "workspace/didRenameFiles", "workspace/willRenameFiles" },
+    -- }
+    -- Configure specific server options
+    -- return {
+    --   keymaps = key,
+    --   servers = {
+    --     tsserver = {
+    --       keys = {
+    --         { "<leader>co", "<cmd>TypescriptOrganizeImports<CR>", desc = "Organize Imports" },
+    --         { "<leader>cR", "<cmd>TypescriptRenameFile<CR>", desc = "Rename File" },
+    --       },
+    --     },
+    --   },
+    -- }
+  end,
+}
+
+-- { "gd", vim.lsp.buf.definition, desc = "Goto Definition", has = "definition" },
+-- { "gr", vim.lsp.buf.references, desc = "References", nowait = true },
+-- { "gI", vim.lsp.buf.implementation, desc = "Goto Implementation" },
+-- { "gy", vim.lsp.buf.type_definition, desc = "Goto T[y]pe Definition" },
+-- { "gD", vim.lsp.buf.declaration, desc = "Goto Declaration" },
+-- { "K", function() return vim.lsp.buf.hover() end, desc = "Hover" },
+-- { "gK", function() return vim.lsp.buf.signature_help() end, desc = "Signature Help", has = "signatureHelp" },
+-- { "<c-k>", function() return vim.lsp.buf.signature_help() end, mode = "i", desc = "Signature Help", has = "signatureHelp" },
+-- { "<leader>ca", vim.lsp.buf.code_action, desc = "Code Action", mode = { "n", "v" }, has = "codeAction" },
+-- { "<leader>cc", vim.lsp.codelens.run, desc = "Run Codelens", mode = { "n", "v" }, has = "codeLens" },
+-- { "<vim.cmd("resize | vertical resize")>cC", vim.lsp.codelens.refresh, desc = "Refresh & Display Codelens", mode = { "n" }, has = "codeLens" },
+-- { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File", mode ={"n"}, has = { "workspace/didRenameFiles", "workspace/willRenameFiles" } },
+-- { "<leader>cr", vim.lsp.buf.rename, desc = "Rename", has = "rename" },
+-- { "<leader>cA", LazyVim.lsp.action.source, desc = "Source Action", has = "codeAction" },
