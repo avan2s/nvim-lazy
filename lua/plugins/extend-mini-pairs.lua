5cc79a723c120afabb53f66a8521d924ebcf2547
avh <andreas.vanhelden@conceto.de>
2025-06-29 17:02:50 +0200

    20250629-replace-nvim: configure mini-pairs

---
+++ lua/plugins/extend-mini-pairs.lua
@@ -0,0 +1,12 @@
+return {
+  "echasnovski/mini.pairs",
+  opts = {
+    -- Remove specific pairs from auto-completion
+    mappings = {
+      ["("] = false,
+      ['"'] = false,
+      ["'"] = false,
+      ["`"] = false,
+    },
+  },
+}
