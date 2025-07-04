bda4a873fdfc502aa8e600071edffefdea1eed89
avh <andreas.vanhelden@conceto.de>
2025-07-04 02:51:40 +0200

    20250629-replace-nvim: add comment with floating window style

--- lua/plugins/extend-diagnostic-keybindings.lua
+++ lua/plugins/extend-diagnostic-keybindings.lua
@@ -33,4 +33,19 @@ return {
       desc = "Diagnostics: Toggle Float / Focus",
     },
   },
+  -- this makes the whole window instead of the diagnostic window in that style - strange - commented out for now
+  -- opts = {
+  --   -- This section is not used, but the `config` function will run
+  -- },
+  -- config = function()
+  --   -- Configure the appearance of the diagnostic float
+  --   vim.diagnostic.config({
+  --     float = {
+  --       border = "rounded", -- "none", "single", "double", "rounded", "solid", "shadow"
+  --       source = "if_many", -- "if_many" (default) or true/false to always/never show the source
+  --       header = "", -- Don't show a header
+  --       prefix = "", -- Don't show a prefix
+  --     },
+  --   })
+  -- end,
 }
