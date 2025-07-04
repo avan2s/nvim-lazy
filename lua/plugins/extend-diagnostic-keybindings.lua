291fd27e1019e8adf020a782b1bf60c5a5e1d228
avh <andreas.vanhelden@conceto.de>
2025-07-04 02:29:38 +0200

    20250629-replace-nvim: add X for showing up the diagnostics

---
+++ lua/plugins/extend-diagnostic-keybindings.lua
@@ -0,0 +1,36 @@
+-- lua/plugins/custom-keymaps.lua
+return {
+  -- This is a dummy plugin spec. We are only using it to install our keymaps.
+  "LazyVim/LazyVim",
+  keys = {
+    {
+      "X",
+      function()
+        -- This variable will hold the window ID of the diagnostic float
+        -- 'vim.b.diagnostic_winid' stores it per-buffer, which is a good practice.
+        -- We use a buffer-local variable to avoid conflicts between different files.
+        local diagnostic_winid = vim.b.diagnostic_winid
+
+        -- Check if the window ID we stored is still a valid, open window
+        if diagnostic_winid and vim.api.nvim_win_is_valid(diagnostic_winid) then
+          -- If it is, this is the "second press": move the cursor into it
+          vim.api.nvim_set_current_win(diagnostic_winid)
+        else
+          -- If not, this is the "first press": open a new float
+          -- The 'scope = "cursor"' option ensures it only shows diagnostics for the current line
+          -- 'focusable = true' is crucial so we can move into the window later
+          local _, winid = vim.diagnostic.open_float(nil, {
+            scope = "cursor",
+            focusable = true,
+          })
+          -- If a window was successfully opened, store its ID
+          if winid then
+            vim.b.diagnostic_winid = winid
+          end
+        end
+      end,
+      mode = "n", -- Normal mode
+      desc = "Diagnostics: Toggle Float / Focus",
+    },
+  },
+}
