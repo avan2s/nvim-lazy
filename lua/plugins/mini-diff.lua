aa16b81aafa67c5ff21109a04cb196c9565ef756
avh <andreas.vanhelden@conceto.de>
2025-07-06 02:40:13 +0200

    20250629-replace-nvim: change colorschema to night

--- lua/plugins/mini-diff.lua
+++ lua/plugins/mini-diff.lua
@@ -27,4 +27,17 @@ return {
       },
     },
   },
+  config = function(_, opts)
+    require("mini.diff").setup(opts)
+
+    -- Set up highlight groups for better visibility
+    -- vim.api.nvim_set_hl(0, "MiniDiffSignAdd", { fg = "#50fa7b" })
+    vim.api.nvim_set_hl(0, "MiniDiffSignChange", { fg = "#f1fa8c" })
+    -- vim.api.nvim_set_hl(0, "MiniDiffSignDelete", { fg = "#ff5555" })
+
+    -- For overlay mode - make changed lines have yellow background
+    vim.api.nvim_set_hl(0, "MiniDiffOverChange", { bg = "#f1fa8c", fg = "#282a36" })
+    -- vim.api.nvim_set_hl(0, "MiniDiffOverAdd", { bg = "#50fa7b", fg = "#282a36", blend = 20 })
+    -- vim.api.nvim_set_hl(0, "MiniDiffOverDelete", { bg = "#ff5555", fg = "#f8f8f2" })
+  end,
 }
