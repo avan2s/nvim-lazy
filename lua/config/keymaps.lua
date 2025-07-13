21f0765642380a8ce121b48e8238934442348866
avh <andreas.vanhelden@conceto.de>
2025-07-14 00:36:58 +0200

    20250629-replace-nvim: add shortcut jk for accessing terminal in nvim mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -40,3 +40,6 @@ keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height
 keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
 keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
 keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+-- more easy terminal vim mode access
+keymap.set("t", "jk", "<C-\\><C-n>", { desc = "TERMINAL • Go into Terminal Nvim Mode" })
