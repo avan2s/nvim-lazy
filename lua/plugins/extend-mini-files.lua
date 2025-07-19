10e23795fe999b2e3aac57b5c0a5831577c59650
avh <andreas.vanhelden@conceto.de>
2025-07-19 10:44:31 +0200

    20250629-replace-nvim: adjust minifiles setup to also show preview in 13zoll laptop mode, uninstall dap

--- lua/plugins/extend-mini-files.lua
+++ lua/plugins/extend-mini-files.lua
@@ -2,14 +2,21 @@
 return {
   "echasnovski/mini.files",
   opts = {
+    options = {
+      use_as_default_explorer = true,
+    },
+    mappings = {
+      go_out = "<M-h>",
+      go_in = "<M-l>",
+    },
     windows = {
       preview = true,
       -- Increase focused window width to show full filenames
-      width_focus = 50,
+      width_focus = 45,
       -- -- Keep non-focused windows narrow
       width_nofocus = 30,
       -- -- Make preview window take up remaining space
-      width_preview = 80,
+      width_preview = 70,
     },
   },
 }
