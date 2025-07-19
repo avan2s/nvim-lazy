7dddac7992e073144e80fe6338624d9d9e843f2f
avh <andreas.vanhelden@conceto.de>
2025-07-19 23:04:23 +0200

    main: configure enter to go in with mini files

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
   -- { "mfussenegger/nvim-dap", enabled = false },
   -- { "echasnovski/mini.operators", enabled = false },
   --  { "folke/snacks.nvim", enabled = false },
+  -- { "akinsho/bufferline.nvim", enabled = false },
 }
--- lua/plugins/extend-mini-files.lua
+++ lua/plugins/extend-mini-files.lua
@@ -1,13 +1,45 @@
 -- ~/.config/nvim/lua/plugins/extend-mini-files.lua
 return {
   "echasnovski/mini.files",
+  keys = {
+    {
+      "<CR>",
+      function()
+        require("mini.files").go_in()
+      end,
+      desc = "Go in entry",
+    },
+    -- {
+    --   "<leader>e",
+    --   function()
+    --     require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+    --   end,
+    --   desc = "Open mini.files (directory of current file)",
+    -- },
+    -- {
+    --   "<leader>E",
+    --   function()
+    --     require("mini.files").open(vim.uv.cwd(), true)
+    --   end,
+    --   desc = "Open mini.files (cwd)",
+    -- },
+    -- {
+    --   "<leader>fm",
+    --   function()
+    --     require("mini.files").open(LazyVim.root(), true)
+    --   end,
+    --   desc = "Open mini.files (root)",
+    -- },
+  },
   opts = {
     options = {
       use_as_default_explorer = true,
     },
     mappings = {
       go_out = "<M-h>",
+      go_out_plus = "<C-h>",
       go_in = "<M-l>",
+      go_in_plus = "<C-l>",
     },
     windows = {
       preview = true,
