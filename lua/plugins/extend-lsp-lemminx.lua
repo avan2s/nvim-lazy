9587e78257dfafed4684137308b0c078d35c620a
avh <andreas.vanhelden@conceto.de>
2025-07-23 00:55:54 +0200

    add-xml-lemminx-support: add lemminx for xml support

---
+++ lua/plugins/extend-lsp-lemminx.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    -- Use the 'opts' function to safely modify the existing options
+    opts = {
+      ensure_installed = { "lemminx" },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        lemminx = {
+          settings = {
+            catalogs = { vim.fn.stdpath("config") .. "/catalog.xml" },
+          },
+        },
+      },
+    },
+  },
+}
