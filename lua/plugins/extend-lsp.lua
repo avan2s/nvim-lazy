297944547e594d66c81982b0dd0ce65f19f6e5ed
avh <andreas.vanhelden@conceto.de>
2026-07-16 09:06:06 +0200

    prevent warning when unstaging files in diffview

--- lua/plugins/extend-lsp.lua
+++ lua/plugins/extend-lsp.lua
@@ -27,6 +27,20 @@ return {
           },
         },
         cssls = {},
+        marksman = {
+          --[[
+            Skip diffview/non-file buffers (e.g. ".git/:0:/README.md").
+            Marksman crashes in closeDoc when such a buffer is closed
+            (staging in diffview), trying to re-read a path that isn't on disk.
+          ]]
+          root_dir = function(bufnr, on_dir)
+            local fname = vim.api.nvim_buf_get_name(bufnr)
+            if fname:find("://") or fname:find("/%.git/") then
+              return
+            end
+            on_dir(vim.fs.root(bufnr, { ".marksman.toml", ".git" }) or vim.fn.fnamemodify(fname, ":p:h"))
+          end,
+        },
         vtsls = {
           settings = {
             complete_function_calls = false,
