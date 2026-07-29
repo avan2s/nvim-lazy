d39c30f403f9fa20e96daa7c61fc1f53bd7fd782
avh <andreas.vanhelden@conceto.de>
2026-07-29 13:46:55 +0200

    add ghm command for comparing with main branch

--- lua/plugins/extend-gitsigns.lua
+++ lua/plugins/extend-gitsigns.lua
@@ -1,3 +1,15 @@
+local function base_branch()
+  local cwd = vim.fs.dirname(vim.api.nvim_buf_get_name(0))
+  for _, branch in ipairs({ "develop", "main", "master" }) do
+    for _, ref in ipairs({ "refs/heads/" .. branch, "refs/remotes/origin/" .. branch }) do
+      local ok = vim.system({ "git", "rev-parse", "--verify", "--quiet", ref }, { cwd = cwd }):wait()
+      if ok.code == 0 then
+        return ref
+      end
+    end
+  end
+end
+
 return {
   "lewis6991/gitsigns.nvim",
   opts = {
@@ -18,6 +30,18 @@ return {
         require("gitsigns").diffthis()
       end,
       desc = "Open Changes",
+    },
+    {
+      "<leader>ghm",
+      function()
+        local base = base_branch()
+        if not base then
+          vim.notify("No develop/main/master branch found", vim.log.levels.WARN)
+          return
+        end
+        require("gitsigns").diffthis(base)
+      end,
+      desc = "Diff This (develop/main)",
     },
     {
       "<leader>ghP",
